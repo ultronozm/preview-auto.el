fcbd9f12c323da336089bf008920c0a290126393
Paul Nelson <ultrono@gmail.com>
2024-10-29 06:54:33 +0100

    bump version to 0.4

--- preview-auto.el
+++ preview-auto.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024  Free Software Foundation, Inc.
 
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
-;; Version: 0.3
+;; Version: 0.4
 ;; URL: https://github.com/ultronozm/preview-auto.el
 ;; Package-Requires: ((emacs "29.3") (auctex "14.0.5"))
 ;; Keywords: tex, convenience
